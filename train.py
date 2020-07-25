f1b5818a4ecf30254791de795df4bbaf4d3fdc10
chris <christopher.melen@rncm.ac.uk>
2020-07-25 20:38:39 +0100

    Reinstated the epsilon value of 1e-4 in the Adam optimizer. It seems that without this training can't progress very far, in tests not being able to get higher than 40-50% accuracy. This setting was taken from WaveNet.

--- train.py
+++ train.py
@@ -101,19 +101,17 @@ def get_arguments():
 # https://github.com/ibab/tensorflow-wavenet/blob/master/wavenet/ops.py
 
 def create_adam_optimizer(learning_rate, momentum):
-    return tf.optimizers.Adam(learning_rate=learning_rate)
-
+    return tf.optimizers.Adam(learning_rate=learning_rate,
+                              epsilon=1e-4)
 
 def create_sgd_optimizer(learning_rate, momentum):
     return tf.optimizers.SGD(learning_rate=learning_rate,
                              momentum=momentum)
 
-
 def create_rmsprop_optimizer(learning_rate, momentum):
     return tf.optimizers.RMSprop(learning_rate=learning_rate,
                                  momentum=momentum,
                                  epsilon=1e-5)
-
 
 optimizer_factory = {'adam': create_adam_optimizer,
                      'sgd': create_sgd_optimizer,
